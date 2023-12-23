18712b8b82ca23b14cd33a074516792eeeb7f5f0
Bartolo72 <bartosz.kosinski.b@gmail.com>
2023-12-23 12:11:20 +0100

    fixed db env file creation, check images if outdated before pull, changed container network to host

--- packetbase_v0.1.sh
+++ packetbase_v0.1.sh
@@ -2,13 +2,13 @@
 
 # Default values
 ES_MEM_LIMIT=4294967296
-ENCRYPTION_KEY=c34d38b3a14956121ff2170e5030b471551370178f43e5626eec58b04a30fae2
 ES_VERSION=7.10.2
 DATABASE_HOST=es01
-DATABASE_USER=root
-DATABASE_PASSWORD=root
+DATABASE_USER=elastic
 ML_API=${NEXT_PUBLIC_ML_API:-localhost}
 DB_API=${NEXT_PUBLIC_DB_API:-localhost}
+DB=${DB:-localhost}
+DB_PORT=${DB_PORT:-9200}
 ML_API_PORT=${NEXT_PUBLIC_ML_API_PORT:-8080}
 DB_API_PORT=${NEXT_PUBLIC_DB_API_PORT:-8000}
 
@@ -28,15 +28,32 @@ do
         DB_API_PORT=${DB_API_ARG[1]}
         shift
         ;;
+        --elastic=*)
+        IFS=':' read -ra DB_ARG <<< "${arg#*=}"
+        DB=${DB_ARG[0]}
+        DB_PORT=${DB_ARG[1]}
+        shift
+        ;;
     esac
 done
 
+echo -e "Welcome to \e[94m_PACKET_BASE\e[1m!"
+echo -e "\e[30m\e[0m"
+sleep 0.3
+
 # User input
+echo "Please provide elastic password which will be used for database authentication"
 read -p "Enter Elastic Password: " ELASTIC_PASSWORD
+sleep 0.3
+
 read -p "Enter cluster name: " CLUSTER_NAME
-read -p "Enter Elastic Port: " ES_PORT
+sleep 0.3
+
+echo "Please provide default username and password for packetbase."
 read -p "Enter default username: " NEXT_PUBLIC_USERNAME
 read -p "Enter default password: " NEXT_PUBLIC_PASSWORD
+sleep 0.3
+echo
 
 
 # Get network interface for packet sniffer
@@ -80,9 +97,8 @@ cat > .db_env << EOF
 ELASTIC_PASSWORD=$ELASTIC_PASSWORD
 CLUSTER_NAME=$CLUSTER_NAME
 ES_VERSION=$ES_VERSION
-ES_PORT=$ES_PORT
+ES_PORT=$DB_PORT
 ES_MEM_LIMIT=$ES_MEM_LIMIT
-ENCRYPTION_KEY=$ENCRYPTION_KEY
 EOF
 
 # Create .frontend_env file
@@ -101,7 +117,8 @@ echo "API_PORT=$ML_API_PORT" > .ml_api_env
 # Create .db_api_env file
 cat > .db_api_env << EOF
 DATABASE_HOST=$DATABASE_HOST
-DATABASE_PORT=$ES_PORT
+DATABASE_PORT=$DB_PORT
+DATABASE=$DB
 DATABASE_USER=$DATABASE_USER
 DATABASE_PASSWORD=$DATABASE_PASSWORD
 API_PORT=$DB_API_PORT
@@ -117,23 +134,84 @@ INTERFACE=$INTERFACE
 EOF
 
 # Docker Cleanup
+echo "Starting Docker cleanup"
+echo "Removing tangling containers..."
+docker image prune -f # Remove tangling containers
+echo "Finished"
 
-docker image prune # Remove tangling containers
-docker rm $(docker ps -a -f status=exited -q) # Remove exited containers
+if [[ "$(docker ps -a -q 2> /dev/null)" != "" ]]; then
+  echo "Removing all exited containers..."
+  docker stop $(docker ps -a -q)
+  docker rm $(docker ps -a -q) # Remove exited containers
+  echo "Finished"
+fi
+
+
+check_and_update_image() {
+  local image=$1
+  echo "Checking for updates for $image..."
+
+  # Pull the latest image digest from the registry
+  local latest_digest=$(docker pull --quiet "$image" 2>/dev/null | tail -1)
+
+  # Get the current image digest
+  local current_digest=$(docker images --no-trunc --quiet "$image" 2>/dev/null)
+
+  if [[ "$latest_digest" != "$current_digest" ]]; then
+    echo "The image $image is outdated. Updating..."
+    docker pull "$image"
+    echo "Updated image: $image"
+  else
+    echo "The image $image is up-to-date."
+  fi
+}
+
+images=(
+  "packetbase/frontend:latest"
+  "packetbase/ml-api:latest"
+  "packetbase/db-api:latest"
+  "packetbase/db:latest"
+  "packetbase/sniffer:latest"
+)
 
 # Pull Docker images
-docker pull packetbase/frontend:latest
-docker pull packetbase/db-api:latest
-docker pull packetbase/ml-api:latest
-docker pull packetbase/db:latest
-docker pull packetbase/sniffer:latest
+echo "Pulling docker images..."
+for image in "${images[@]}"; do
+  if [[ "$(docker images -q $image 2> /dev/null)" == "" ]]; then
+    echo "Image: $image does not exist locally. Pulling..."
+    docker pull "$image"
+    echo "Pulled image: $image"
+  else
+    check_and_update_image "$image"
+  fi
+done
+echo
+sleep 0.3
 
 # Set up netowrk
+echo "Creating docker network..."
 docker network create es-net
+sleep 0.3
+echo
 
 # Run Docker images inside created network
-docker run -d --network es-net packetbase/frontend --env-file ./frontend_env
-docker run -d --network es-net packetbase/db-api --env-file ./db_api_env
-docker run -d --network es-net packetbase/ml-api --env-file ./ml_api_env
-docker run -d --network es-net packetbase/db --env-file ./db_env
-docker run -d --network es-net packetbase/sniffer --env-file ./sniffer_env
+echo "Running docker containers..."
+docker run -d --network es-net --env-file ./.frontend_env -p 3000:3000 packetbase/frontend
+docker run -d --network es-net --env-file ./.db_api_env -p $NEXT_PUBLIC_DB_API_PORT:$NEXT_PUBLIC_DB_API_PORT packetbase/db-api
+docker run -d --network es-net --env-file ./.ml_api_env -p $NEXT_PUBLIC_ML_API_PORT:$NEXT_PUBLIC_ML_API_PORT packetbase/ml-api
+docker run -d --network es-net --env-file ./.db_env \
+    --name es01 \
+    --hostname es01 \
+    -e discovery.type=single-node \
+    --volume es-data-01:/usr/share/elasticsearch/data \
+    --ulimit memlock=-1:-1 \
+    --ulimit nofile=65536:65536 \
+    --memory ${ES_MEM_LIMIT} \
+    -p $DB_PORT:$DB_PORT \
+    -p 9300:9300 \
+    packetbase/db
+docker run -d --network=host --env-file ./.sniffer_env \
+    --privileged \
+    --cap-add=NET_ADMIN \
+    --cap-add=NET_RAW \
+    packetbase/sniffer
